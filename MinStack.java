8e43d815c49a07db6a349f5ebede1498d7c78dd3
naval41 <49112134+naval41@users.noreply.github.com>
2020-07-26 14:58:03 +0530

    Create MinStack.java

---
+++ MinStack.java
@@ -0,0 +1,25 @@
+class MinStack {
+    Stack<Integer> min = new Stack<>();    //min stack
+    Stack<Integer> stack = new Stack<>();  //data stack
+    
+    public void push(int x) {
+        stack.push(x);
+        if (min.isEmpty() || min.peek() >= x) {
+            min.push(x);
+        }
+    }
+
+    public void pop() {
+        if (stack.pop().equals(min.peek())) {
+            min.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min.peek();
+    }
+}
